926cc704511bf045265cc2bc8be1b26498d49b02
JonathanOrtz <4ortij35@solent.ac.uk>
2022-01-21 02:48:05 +0000

    controller and models with ejs templade partially done

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
-
+const morgan = require("morgan");
+const bodyparser = require("body-parser");
+const Duty = require("./models/Duties");
 
 // creating a express object
 const app = express();
@@ -29,12 +31,28 @@ mongoose.connection.on("error", (err) => {
 // setting express for static files
 app.use(express.static(path.join(__dirname, "public")));
 
+// middlewares
+app.use(morgan('dev'));
+app.use(bodyparser.urlencoded({ extended: false}));
+
 
 // routing web pages
 app.get("/", (req, res)=>{
     res.render("index")
 });
 
+app.get("/dutylist", async(req, res) =>{
+    const dutieslist = await Duty.find();
+    console.log(dutieslist)
+    res.render("dutylist", { dutieslist: dutieslist});
+});
+
+app.post("/duties/add", async(req, res)=>{
+    const duty = Duty(req.body);
+    const dutySaved = await duty.save();
+    console.log(dutySaved);
+    res.redirect("/");
+});
 
 
 
